38f3fd9d70baf05108ab1d0f823a03b6115e217a
Tejas <futureautomate@gmail.com>
2024-08-18 22:53:03 +0530

    Add existing project files to Git

---
+++ NZWalks.API/Controllers/StudentsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NZWalks.API.Controllers
+{
+
+    // https://localhost:portnumber/api/students
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        // Get: https://localhost:portnumber/api/students
+        [HttpGet]
+        public IActionResult GetAllStudents()
+        {
+            string[] students = new string[] { "Tejas","Aaron","Berry","David"};
+
+            return Ok(students);
+        }
+    }
+}
